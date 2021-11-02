738c852ff6a9ab02314f4ee658a1754d474c2c31
Houssam OUATMANI <ouatmanihoussam@gmail.com>
2021-11-02 21:18:51 +0100

    Appli meteo avec syntax import

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,8 @@
+"use strict";
+
 import api from "./utils/api.js";
 import { CONTAINER, FORM, INPUT } from "./utils/domElements.js";
-import { round } from "./utils/round.js";
+import { round, ternaire } from "./utils/round.js";
 
 const { baseName, baseCoords, key, lang, units } = api;
 
@@ -12,7 +14,6 @@ const success = (position) => {
   const {
     coords: { latitude, longitude },
   } = position;
-
   const URL = `${baseCoords}${latitude}&lon=${longitude}&lang=${lang}&units=${units}&appid=${key}`;
   getCurrentWeatherBasedOnLocation(URL).then((wData) => displayData(wData));
 };
@@ -22,6 +23,55 @@ const error = (error) => {
     <h2 style="text-align:center;">You need to Enable Location</h2>
  </div>`;
 };
+const getCurrentWeatherBasedOnLocation = async (URL) => {
+  try {
+    const response = await fetch(URL);
+    const wData = await response.json();
+    return wData;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+navigator.geolocation.getCurrentPosition(success, error);
+
+const getCurrentWeatherBasedOnInput = async () => {
+  const URL = `${baseName}${INPUT.value}&lang=${lang}&units=${units}&appid=${key}`;
+  try {
+    const response = await fetch(URL);
+    const wData = await response.json();
+    INPUT.value = "";
+    return wData;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const displayData = (wData) => {
+  CONTAINER.innerHTML = "";
+  const { temp, temp_min, temp_max, feels_like, humidity } = wData.main;
+  CONTAINER.innerHTML = `
+  <div class="description">
+  <h2>${wData.name} ${wData.sys.country}</h2>
+  </div>
+  <div class="details ${ternaire(temp, 12, "detail-hot", "detail-cold")}">
+  <h3>${wData.weather[0].description.toUpperCase()}</h3>
+  <ul>
+  <li>Temperature<span class=${ternaire(temp, 12)}>${round(temp)} °C</span></li>
+  <li>Minimale <span class=${ternaire(temp_min, 12)}>${round(
+    temp_min
+  )} °C</span> </li>
+  <li>Maximale <span class=${ternaire(temp_max, 12)}>${round(
+    temp_max
+  )} °C</span> </li>
+  <li>Ressentie <span class=${ternaire(ternaire, 12)}>${round(
+    feels_like
+  )} °C</span> </li>
+  <li>Humidité <span >${round(humidity)} %</span></li>
+  </ul>
+  </div>
+  `;
+};
 
 FORM.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -29,45 +79,3 @@ FORM.addEventListener("submit", (e) => {
     displayData(wData);
   });
 });
-navigator.geolocation.getCurrentPosition(success, error);
-
-const getCurrentWeatherBasedOnLocation = async (URL) => {
-  const response = await fetch(URL);
-  const wData = await response.json();
-  return wData;
-};
-
-const getCurrentWeatherBasedOnInput = async () => {
-  const URL = `${baseName}${INPUT.value}&lang=${lang}&units=${units}&appid=${key}`;
-  const response = await fetch(URL);
-  const wData = await response.json();
-  INPUT.value = "";
-  return wData;
-};
-const displayData = (wData) => {
-  CONTAINER.innerHTML = "";
-  const { temp, temp_min, temp_max, feels_like, humidity } = wData.main;
-  CONTAINER.innerHTML += `
-        <div class="description">
-            <h2>${wData.name} ${wData.sys.country}</h2>
-        </div>
-        <div class="details ${temp > 32 ? "detail-hot" : "detail-cold"}">
-            <h3>${wData.weather[0].description.toUpperCase()}</h3>
-            <ul>
-                <li>Temperature <span class=${
-                  temp > 12 ? "hot" : "cold"
-                }> ${round(temp)} °C</span> </li>
-                <li>Minimale <span class=${
-                  temp_min > 12 ? "hot" : "cold"
-                }>${round(temp_min)} °C</span> </li>
-                <li>Maximale <span class=${
-                  temp_max > 12 ? "hot" : "cold"
-                }>${round(temp_max)} °C</span> </li>
-                <li>Ressenti <span class=${
-                  feels_like > 12 ? "hot" : "cold"
-                }>${round(feels_like)} °C</span> </li>
-                <li>Humidité <span >${round(humidity)} %</span></li>
-            </ul>
-        </div>
-    `;
-};
--- src/scripts/utils/round.js
+++ src/scripts/utils/round.js
@@ -1,1 +1,5 @@
-export const round = (temp) => Math.round(temp) 
+export const round = (temp) => Math.round(temp);
+
+export const ternaire = (arg1, ar2, class1 = "hot", class2 = "cold") => {
+  return arg1 > ar2 ? class1 : class2;
+};
